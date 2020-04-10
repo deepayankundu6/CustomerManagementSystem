5f35c6149ac670098cb422a95b52311851e79bfe
Deepayan Kundu <deepayankundu6@gmail.com>
2020-04-10 11:53:23 +0530

    changes

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -2,9 +2,9 @@ var path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './foo.js',
+    entry: './app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'foo.bundle.js'
+        filename: 'api.js'
     }
 };
