59941e1cb3f6cb49504d85cb15d8998d7ad10b79
Deepayan Kundu <deepayankundu6@gmail.com>
2020-04-10 12:06:26 +0530

    cngs

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,8 +1,14 @@
 var path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     mode: 'development',
     entry: './app.js',
+    target: 'node',
+    externals: [nodeExternals()],
+    devServer: {
+        port: 5000
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'api.js'
