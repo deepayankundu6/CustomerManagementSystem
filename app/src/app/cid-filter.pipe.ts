2b577df5ad8cf9b43fe88e1e41b6a680ea76376e
Deepayan Kundu <deepayankundu6@gmail.com>
2020-04-14 11:37:25 +0530

    filtering done

--- app/src/app/cid-filter.pipe.ts
+++ app/src/app/cid-filter.pipe.ts
@@ -1,11 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { ICustomers } from './icustomers';
 
 @Pipe({
   name: 'cidFilter'
 })
 export class CidFilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(values: ICustomers[], filter: string): ICustomers[] {
+    if (values && filter && values.length > 0) {
+
+      values = values.filter((el: ICustomers) => {
+        if (el.CustomerID.toString().includes(filter))
+          return values;
+      }
+      )
+      return values;
+    }
+    else {
+      return values;
+    }
   }
 }
--- app/src/app/customer-view/customer-view.component.ts
+++ app/src/app/customer-view/customer-view.component.ts
@@ -3,14 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from "ngx-spinner";
 import { ICustomers } from "../icustomers";
+import { CidFilterPipe } from '../cid-filter.pipe';
 @Component({
   selector: 'app-customer-view',
   templateUrl: './customer-view.component.html',
   styleUrls: ['./customer-view.component.css']
 })
 export class CustomerViewComponent implements OnInit {
-  customers: ICustomers;
-  SearchCustomer: String;
+  customers: ICustomers[];
+  SearchCustomer: string;
   constructor(private http: HttpClient, private toastr: ToastrService, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
@@ -19,7 +20,7 @@ export class CustomerViewComponent implements OnInit {
 
   getCustomers() {
     this.spinner.show();
-    this.http.get("/api/customer/getall").subscribe((customers: ICustomers) => {
+    this.http.get("/api/customer/getall").subscribe((customers: ICustomers[]) => {
       if (customers) {
         this.customers = customers;
       } else {
@@ -34,7 +35,6 @@ export class CustomerViewComponent implements OnInit {
     let payload = {
       "CustomerID": cid
     }
-    console.log("Delete:", payload);
     this.spinner.show();
     this.http.post("/api/customer/delete", payload).subscribe((resp) => {
       this.toastr.success("Success", "Customer removed successfully");
