18fbdb42bb99e5594d1dc5288d54203e8641e6b5
Deepayan Kundu <deepayankundu6@gmail.com>
2020-04-16 12:25:45 +0530

    work done

--- app/src/app/editcustomerdetails/editcustomerdetails.component.ts
+++ app/src/app/editcustomerdetails/editcustomerdetails.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editcustomerdetails',
@@ -7,7 +12,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditcustomerdetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private toastr: ToastrService,
+    private spinner: NgxSpinnerService, private dialogReff: MatDialogRef<EditcustomerdetailsComponent>,
+    private activatedRoute: ActivatedRoute, @Inject(MAT_DIALOG_DATA) public customer
+  ) { }
 
   ngOnInit(): void {
   }
