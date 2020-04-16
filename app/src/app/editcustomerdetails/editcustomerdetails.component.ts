23d4bc00ac63dc6de85fbddb9d1c1f2b0db2cfb3
Deepayan Kundu <deepayankundu6@gmail.com>
2020-04-16 13:12:47 +0530

    edit form left

--- app/src/app/editcustomerdetails/editcustomerdetails.component.ts
+++ app/src/app/editcustomerdetails/editcustomerdetails.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -12,12 +13,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditcustomerdetailsComponent implements OnInit {
 
+
   constructor(private http: HttpClient, private toastr: ToastrService,
-    private spinner: NgxSpinnerService, private dialogReff: MatDialogRef<EditcustomerdetailsComponent>,
+    private spinner: NgxSpinnerService, private dialogRef: MatDialogRef<EditcustomerdetailsComponent>,
     private activatedRoute: ActivatedRoute, @Inject(MAT_DIALOG_DATA) public customer
   ) { }
 
   ngOnInit(): void {
   }
 
+  onSubmitClick() {
+    console.log("API Call");
+    this.dialogRef.close();
+
+  }
+  onCancel() {
+    this.dialogRef.close();
+  }
 }
