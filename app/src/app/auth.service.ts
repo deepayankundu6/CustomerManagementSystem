2180fa96f0891fa4a91cacb8f042a984d7cc0bdd
Deepayan Kundu <deepayankundu6@gmail.com>
2020-04-26 21:31:21 +0530

    done

--- app/src/app/auth.service.ts
+++ app/src/app/auth.service.ts
@@ -10,10 +10,18 @@ export class AuthService implements CanActivate {
     this.valid = false;
   }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+
+    if (!this.valid) {
+      alert("Please Login First");
+      this._router.navigateByUrl('')
+    }
     return this.valid;
   }
 
   login() {
     this.valid = true;
+  }
+  logout() {
+    this.valid = false;
   }
 }
